0116e5880073943bb5aa2eddde947fb819e9b2fb
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-05-29 13:07:20 +0530

    useref added

--- Projects/ToDo/src/components/AddTodo.jsx
+++ Projects/ToDo/src/components/AddTodo.jsx
@@ -1,24 +1,20 @@
-import { useState } from "react";
+import { useRef } from "react";
 import { IoBagAdd } from "react-icons/io5";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState("");
-  const [dueDate, setDueDate] = useState("");
-
-  const handleNameChange = (e) => {
-    setTodoName(e.target.value);
-  };
-
-  const handleDateChange = (e) => {
-    setDueDate(e.target.value);
-  };
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
 
   const handleAddNewButton = (event) => {
     event.preventDefault();
-    console.log(event);
+
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
+
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
   };
 
   return (
@@ -29,12 +25,11 @@ function AddTodo({ onNewItem }) {
             <input
               type="text"
               placeholder="Enter your task"
-              value={todoName}
-              onChange={handleNameChange}
+              ref={todoNameElement}
             />
           </div>
           <div className="col-4">
-            <input type="date" value={dueDate} onChange={handleDateChange} />
+            <input type="date" ref={dueDateElement} />
           </div>
           <div className="col-2" style={{ display: "flex" }}>
             <button className="btn btn-success custom-button">
