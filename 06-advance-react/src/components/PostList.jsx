597cdd7789964b06c032309d81db85c7dd2a0152
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-05-31 19:41:46 +0530

    fetch in react

--- 06-advance-react/src/components/PostList.jsx
+++ 06-advance-react/src/components/PostList.jsx
@@ -1,12 +1,25 @@
 import React, { useContext } from "react";
 import Post from "./Post";
 import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
-  const { postList } = useContext(PostListData);
+  const { postList, addInitialPosts } = useContext(PostListData);
+
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+      });
+  };
+
   console.log(postList);
   return (
     <>
+      {postList.length === 0 && (
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+      )}
       {postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
--- 06-advance-react/src/store/post-list-store.jsx
+++ 06-advance-react/src/store/post-list-store.jsx
@@ -3,6 +3,7 @@ import { createContext, useReducer } from "react";
 const DEFAULT_CONTEXT = {
   postList: [],
   addPost: () => {},
+  addInitialPosts: () => {},
   deletePost: () => {},
 };
 
@@ -15,6 +16,8 @@ const postListReducer = (currPostList, action) => {
     newPostList = currPostList.filter(
       (post) => post.id !== action.payload.postId
     );
+  } else if (action.type === "ADD_INITIAL_POSTS") {
+    newPostList = action.payload.posts;
   } else if (action.type === "ADD_POST") {
     newPostList = [action.payload, ...currPostList];
   }
@@ -22,10 +25,7 @@ const postListReducer = (currPostList, action) => {
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(
-    postListReducer,
-    DEFAULT_POST_LIST
-  );
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
 
   const addPost = (userId, postTitle, postBody, reactions, tags) => {
     dispatchPostList({
@@ -41,6 +41,15 @@ const PostListProvider = ({ children }) => {
     });
   };
 
+  const addInitialPosts = (posts) => {
+    dispatchPostList({
+      type: "ADD_INITIAL_POSTS",
+      payload: {
+        posts,
+      },
+    });
+  };
+
   const deletePost = (postId) => {
     dispatchPostList({
       type: "DELETE_POST",
@@ -51,29 +60,12 @@ const PostListProvider = ({ children }) => {
   };
 
   return (
-    <PostList.Provider value={{ postList, addPost, deletePost }}>
+    <PostList.Provider
+      value={{ postList, addPost, addInitialPosts, deletePost }}
+    >
       {children}
     </PostList.Provider>
   );
 };
-
-const DEFAULT_POST_LIST = [
-  {
-    id: "001",
-    title: "Learn Web Development",
-    body: "Web developers use languages like HTML, CSS, JavaScript, and server-side scripting languages like PHP, Python, or Ruby to develop websites and web applications that can be accessed over the internet.",
-    reactions: 5,
-    userId: "user-007",
-    tags: ["WebDev", "HTML", "CSS", "JS"],
-  },
-  {
-    id: "002",
-    title: "Learn Backend Development",
-    body: "Back-end development means working on server-side software, which focuses on everything you can't see on a website.",
-    reactions: 3,
-    userId: "user-005",
-    tags: ["Express", "NodeJS", "JS", "NextJS"],
-  },
-];
 
 export default PostListProvider;
