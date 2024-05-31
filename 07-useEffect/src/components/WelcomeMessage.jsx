43f0c3c4a9e1ae0f18693de5cf3b56230dc78218
Satyajeet Das Chowdhury <satyajeetdc@gmail.com>
2024-05-31 20:59:17 +0530

    useEffect

---
+++ 07-useEffect/src/components/WelcomeMessage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center>
+      <h1 className="welcome-msg">There are no new posts</h1>
+    </center>
+  );
+};
+
+export default WelcomeMessage;
